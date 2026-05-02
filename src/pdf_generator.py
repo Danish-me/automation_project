6c273faf5a0dbdf322e3d473f015265d439a42e6
Danish Mehmood <syedd8896@gmail.com>
2026-05-02 22:57:43 +0500

    update

--- src/pdf_generator.py
+++ src/pdf_generator.py
@@ -84,8 +84,8 @@ def generate_pdf_report(df, status, f0_value, deviations, report_id):
 
     try:
         # 🔹 Select channels dynamically
-        cols = [col for col in df.columns if "channel" in col.lower()]
-
+        # cols = [col for col in df.columns if "channel" in col.lower()]
+        cols = []
         if "date" in df.columns:
                 cols.append("date")
 

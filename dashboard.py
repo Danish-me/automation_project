828a92d0741164250dd0da99bd799e86d8d33917
Danish Mehmood <syedd8896@gmail.com>
2026-05-03 12:49:16 +0500

    update

--- dashboard.py
+++ dashboard.py
@@ -12,13 +12,13 @@ sys.path.append(BASE_DIR)
 sys.path.append(os.path.join(BASE_DIR, "src"))
 
 # ================= IMPORT MODULES =================
-from src.data_reader import read_excel
-from src.data_processor import analyze_batch, get_temperature_columns
-from src.graph_generator import plot_temperature
-from src.report_generator import save_excel_report
-from src.email_sender import send_email_report
-from src.pdf_generator import generate_pdf_report
-from src.audit_logger import log_audit
+from data_reader import read_excel
+from data_processor import analyze_batch, get_temperature_columns
+from graph_generator import plot_temperature
+from report_generator import save_excel_report
+from email_sender import send_email_report
+from pdf_generator import generate_pdf_report
+from audit_logger import log_audit
 
 # ================= CONFIG =================
 st.set_page_config(page_title="Autoclave Dashboard", layout="wide")

d422e48f6c345969eb88aaea56f9e6899e4574da
Danish Mehmood <syedd8896@gmail.com>
2026-05-05 21:19:29 +0500

    final fix import path

--- dashboard.py
+++ dashboard.py
@@ -14,10 +14,8 @@ if ROOT_DIR not in sys.path:
 # ================= PATH SETUP =================
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-SRC_DIR = os.path.join(BASE_DIR, "src")
+# SRC_DIR = os.path.join(BASE_DIR, "src")
 
-if SRC_DIR not in sys.path:
-    sys.path.insert(0, SRC_DIR)
 
 # ================= IMPORT MODULES =================
 from data_reader import read_excel
